c2ddd42566c15077655b33c43caed7fb4f4f7da9
Laurin Neff <laurin@laurinneff.ch>
2022-02-09 17:36:09 +0100

    edit /etc/hosts

--- com.gitlab.KRypt0n_.an-anime-game-launcher.sh
+++ com.gitlab.KRypt0n_.an-anime-game-launcher.sh
@@ -5,5 +5,32 @@ for i in {0..9}; do
     test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
 
+# Modify /etc/hosts to block logging servers
+# This is possible because /etc is a writable tmpfs in flatpak
+if readlink /etc/hosts > /dev/null; then
+    # /etc/hosts is a symlink by default, if it is, copy the original and modify it
+    # Otherwise, we already modified it
+    original=$(readlink /etc/hosts)
+    rm /etc/hosts
+    cp $original /etc/hosts
+    cat <<EOF >> /etc/hosts
+# Genshin logging servers (do not remove!)
+0.0.0.0 log-upload-os.mihoyo.com
+0.0.0.0 overseauspider.yuanshen.com
+EOF
+
+    # If NO_BLOCK_PROXY is set, don't block the proxy/cdn servers
+    if [ -z "$NO_BLOCK_PROXY" ]; then
+        cat <<EOF >> /etc/hosts
+# Optional Unity proxy/cdn servers
+0.0.0.0 prd-lender.cdp.internal.unity3d.com
+0.0.0.0 thind-prd-knob.data.ie.unity3d.com
+0.0.0.0 thind-gke-usc.prd.data.corp.unity3d.com
+0.0.0.0 cdp.cloud.unity3d.com
+0.0.0.0 remote-config-proxy-prd.uca.cloud.unity3d.com
+EOF
+    fi
+fi
+
 cd /app/lib/an-anime-game-launcher
 exec ./an-anime-game-launcher "$@"
