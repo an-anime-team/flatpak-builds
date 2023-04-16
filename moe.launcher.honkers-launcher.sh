53ee51be4184873e7fec1b1bd54cf65e75923f5d
lane <laurin@laurinneff.ch>
2023-04-16 20:50:27 +0200

    honkers launcher 1.0.0

---
+++ moe.launcher.honkers-launcher.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Discord RPC
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
+# Modify /etc/hosts to block logging servers
+# This is possible because /etc is a writable tmpfs in flatpak
+if readlink /etc/hosts > /dev/null; then
+    # /etc/hosts is a symlink by default, if it is, copy the original and modify it
+    # Otherwise, we already modified it
+    original=$(readlink /etc/hosts)
+    rm /etc/hosts
+    cp $original /etc/hosts
+    cat <<EOF >> /etc/hosts
+# Global
+# Honkai Impact 3rd logging servers
+0.0.0.0 log-upload-os.hoyoverse.com
+0.0.0.0 sg-public-data-api.hoyoverse.com
+0.0.0.0 dump.gamesafe.qq.com
+EOF
+fi
+
+export PATH=$PATH:/app/utils/bin
+
+exec honkers-launcher "$@"
