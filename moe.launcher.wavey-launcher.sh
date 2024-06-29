5d2ee15ed42fa78011dbd0f81e55965fe3af910a
Marie <marie@kaifa.ch>
2024-06-29 22:29:11 +0200

    block crashsight

--- moe.launcher.wavey-launcher.sh
+++ moe.launcher.wavey-launcher.sh
@@ -5,6 +5,18 @@ for i in {0..9}; do
     test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
 
+# This is possible because /etc is a writable tmpfs in flatpak
+if readlink /etc/hosts > /dev/null; then
+    # /etc/hosts is a symlink by default, if it is, copy the original and modify it
+    # Otherwise, we already modified it
+    original=$(readlink /etc/hosts)
+    rm /etc/hosts
+    cp $original /etc/hosts
+    cat <<EOF >> /etc/hosts
+0.0.0.0 pc.crashsight.wetest.net
+EOF
+fi
+
 export PATH=$PATH:/usr/lib/extensions/vulkan/gamescope/bin
 
 exec wavey-launcher "$@"
